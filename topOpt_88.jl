2ec9ed688d2982006c1cc90b1eec67579b012bf0
Cristóbal Tapia Camú <crtapia@gmail.com>
2021-01-03 13:29:10 +0100

    chore: new parameters for problem struct

--- topOpt_88.jl
+++ topOpt_88.jl
@@ -18,7 +18,7 @@ struct FilterBase <: Filter; end
 struct FilterConv <: Filter; end
 
 export TopOptProblemBase, TopOptProblemConv
-export topopt
+export minimize
 export line_load!, fix_dof!, symmetry_axis!
 export use_elements!
 
@@ -62,17 +62,19 @@ mutable struct TopOptProblemBase <: TopOptProblem
     nx::Int
     ny::Int
     rmin::Float64
+    volfrac::Float64
+    p::Float64
     dofs::Int
     F::Array{Float64}
     passive::Array{Int,2}
     fixed_dofs::Array{Int}
 
-    function TopOptProblemBase(nx, ny, rmin)
+    function TopOptProblemBase(nx, ny, rmin, volfrac, p)
         dofs::Int = 2 * (nx + 1) * (ny + 1)
         F::Array{Float64} = zeros(dofs)
         passive::Array{Int,2} = zeros(ny, nx)
         fixed_dofs::Array{Int} = Array([])
-        new(nx, ny, rmin, dofs, F, passive, fixed_dofs)
+        new(nx, ny, rmin, volfrac, p, dofs, F, passive, fixed_dofs)
     end
 end
 
@@ -80,17 +82,19 @@ mutable struct TopOptProblemConv <: TopOptProblem
     nx::Int
     ny::Int
     rmin::Float64
+    volfrac::Float64
+    p::Float64
     dofs::Int
     F::Array{Float64}
     passive::Array{Int,2}
     fixed_dofs::Array{Int}
 
-    function TopOptProblemConv(nx, ny, rmin)
+    function TopOptProblemConv(nx, ny, rmin, volfrac, p)
         dofs::Int = 2 * (nx + 1) * (ny + 1)
         F::Array{Float64} = zeros(dofs)
         passive::Array{Int,2} = zeros(ny, nx)
         fixed_dofs::Array{Int} = Array([])
-        new(nx, ny, rmin, dofs, F, passive, fixed_dofs)
+        new(nx, ny, rmin, volfrac, p, dofs, F, passive, fixed_dofs)
     end
 end
 
@@ -146,7 +150,7 @@ end
 TopOptResult(prob::TopOptProblemBase, x) = TopOptResultBase(prob, x)
 TopOptResult(prob::TopOptProblemConv, x) = TopOptResultConv(prob, x)
 
-function topopt(prob::TopOptProblem, volfrac, p; Δ=0.01, filter=1)
+function minimize(prob::TopOptProblem; Δ=0.01, filter=1)
     # Material properties
     E₀ = 1.0
     E_min = 1e-9
@@ -178,7 +182,7 @@ function topopt(prob::TopOptProblem, volfrac, p; Δ=0.01, filter=1)
     # Define FE-problem
     fe_prob = FEProblem(prob.F, U, Kₑ, iK, jK, edofMat, freedofs, nx, ny)
     # Intatiate result object
-    x = volfrac .* ones(ny, nx)
+    x = prob.volfrac .* ones(ny, nx)
     res = TopOptResult(prob, x)
     # Prepare filter
     set_filter!(res, prob, rmin)
@@ -199,15 +203,15 @@ function topopt(prob::TopOptProblem, volfrac, p; Δ=0.01, filter=1)
         loop += 1
         loopbeta += 1
         # FE-analysis
-        solve_fe!(fe_prob, res, p, E₀, E_min)
+        solve_fe!(fe_prob, res.x̃, prob.p, E₀, E_min)
         # Objective function and sensitivity analysis
         cₑ = compliance(fe_prob, E₀, E_min)
-        c = sum((E_min .+ res.x̃.^p .* (E₀ - E_min)) .* cₑ)
+        c = sum((E_min .+ res.x̃.^prob.p .* (E₀ - E_min)) .* cₑ)
         # Update ∂c and ∂V
-        sensitivities!(∂c, ∂V, fe_prob, res, β, p, E₀, E_min, cₑ, filter)
+        sensitivities!(∂c, ∂V, fe_prob, res, β, prob.p, E₀, E_min, cₑ, filter)
         # Optimality criteria update of design variables and physical densities
         # and filtering/modification of sensitivities
-        x_new = optimal_crit(prob, res, volfrac, β, ∂c, ∂V, filter)
+        x_new = optimal_crit(prob, res, β, ∂c, ∂V, filter)
         # Compute the hange
         change = maximum(abs.(x_new - res.x))
         # Update result
@@ -236,7 +240,7 @@ function topopt(prob::TopOptProblem, volfrac, p; Δ=0.01, filter=1)
                     grid=false, axis=false, colorbar=false)
     savefig(plot_1, "plot_88.pdf")
     display(plot_1)
-
+    return res
 end
 
 function set_filter!(res::TopOptResult, prob::TopOptProblemBase, rmin)
@@ -284,7 +288,7 @@ function set_filter!(res::TopOptResult, prob::TopOptProblemConv, rmin)
 end
 
 # Optimal criterion for the normal filter
-function optimal_crit(prob::TopOptProblemBase, res::TopOptResult, volfrac::Float64, β::Float64,
+function optimal_crit(prob::TopOptProblemBase, res::TopOptResult, β::Float64,
                       ∂c::Array{Float64}, ∂V::Array{Float64}, filter)
     nx = prob.nx
     ny = prob.ny
@@ -292,7 +296,7 @@ function optimal_crit(prob::TopOptProblemBase, res::TopOptResult, volfrac::Float
     λ₂::Float64 = 1e9
     move::Float64 = 0.2
     x_new = similar(res.x)
-    tot_volfrac = volfrac * nx * ny
+    tot_volfrac = porb.volfrac * nx * ny
 
     while (λ₂ - λ₁) / (λ₁ + λ₂) > 1e-3
         λₘ = 0.5 * (λ₂ + λ₁);
@@ -322,7 +326,7 @@ function optimal_crit(prob::TopOptProblemBase, res::TopOptResult, volfrac::Float
 end
 
 # Optimal criterion using convolution
-function optimal_crit(prob::TopOptProblemConv, res::TopOptResult, volfrac::Float64, β::Float64,
+function optimal_crit(prob::TopOptProblemConv, res::TopOptResult, β::Float64,
                       ∂c::Array{Float64}, ∂V::Array{Float64}, filter)
     nx = prob.nx
     ny = prob.ny
