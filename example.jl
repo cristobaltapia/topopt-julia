dc9cb8d3839742348184687f2813c259ac78e40d
Cristóbal Tapia Camú <crtapia@gmail.com>
2021-01-03 13:24:07 +0100

    Add example and utils

--- example.jl
+++ example.jl
@@ -1,18 +1,17 @@
 include("topOpt_88.jl")
 using .TopOpt88
 
-nx = 120;
-ny = 200;
+nx = 200;
+ny = 240;
 volfrac = 0.15;
 p = 3.0;
 r_min = nx * 0.03;
 #
-prob = TopOptProblemConv(nx, ny, r_min)
+prob = TopOptProblemConv(nx, ny, r_min, volfrac, p)
 line_load!(prob, ny ÷ 3)
 fix_dof!(prob, :bottom)
 fix_dof!(prob, :right)
 symmetry_axis!(prob, :left)
 use_elements!(prob, :hline; y=ny ÷ 3)
 
-@time topopt(prob, volfrac, p, Δ=0.05, filter=3);
-
+@time res = minimize(prob, Δ=0.01, filter=3);
